f1e56cd901146107451dfb7742c162816d38f187
lightbrca1z <quwieorpt@gmail.com>
2025-04-15 14:17:31 +0900

    HanamiProject-ロゴの反映。.envファイルの削除14:17

--- src/app/routing/kankei/types.ts
+++ src/app/routing/kankei/types.ts
@@ -1,8 +1,11 @@
-import { BaseCard } from '@/components/BusinessCardEditModal';
-
-export interface BusinessCard extends BaseCard {
-  organization: { organizationname: string } | null;
-  region: { regionname: string } | null;
-  category: { categoryname: string } | null;
-  representative: { representativename: string } | null;
+export interface BusinessCard {
+  businesscardid: string;
+  phone?: string | null;
+  mobile?: string | null;
+  email?: string | null;
+  imageurl?: string | null;
+  organization?: { organizationname: string } | null;
+  region?: { regionname: string } | null;
+  category?: { categoryname: string } | null;
+  representative?: { representativename: string } | null;
 } 
